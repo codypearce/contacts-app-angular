9ea05dd3bf1427a24c480c3fa35e8ba726ebc2ab
codypearce <codyapearce@gmail.com>
2017-05-23 20:06:44 -0600

    replace card data with array of objects

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 var app = angular.module('contacts', ['ngMaterial']);
 
 app.controller('mainCtrl', function($scope,  $mdSidenav) {
-  console.log('test')
+  $scope.contacts = [
+    {
+      name: 'Luke Skywalker',
+      phone: '555-555-5555',
+      email: 'skywalkingoveru@rebels.io',
+      img: 'luke-skywalker.jpg',
+    },
+    {
+      name: 'Darth Vader',
+      phone: '555-555-5554',
+      email: 'idontlikesand@empire.io',
+      img: 'darth-vader.jpg',
+    },
+  ];
+
 })
 
 app.controller('LeftCtrl', function($scope) {
