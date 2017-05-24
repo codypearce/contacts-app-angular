8a7a784ae6db3b0f27ede445b67ad6a78190d0ae
codypearce <codyapearce@gmail.com>
2017-05-23 20:11:10 -0600

    separate first and last name

--- js/app.js
+++ js/app.js
@@ -3,13 +3,15 @@ var app = angular.module('contacts', ['ngMaterial']);
 app.controller('mainCtrl', function($scope,  $mdSidenav) {
   $scope.contacts = [
     {
-      name: 'Luke Skywalker',
+      firstName: 'Luke',
+      lastName: 'Skywalker',
       phone: '555-555-5555',
       email: 'skywalkingoveru@rebels.io',
       img: 'luke-skywalker.jpg',
     },
     {
-      name: 'Darth Vader',
+      firstName: 'Darth',
+      lastName: 'Vader',
       phone: '555-555-5554',
       email: 'idontlikesand@empire.io',
       img: 'darth-vader.jpg',
