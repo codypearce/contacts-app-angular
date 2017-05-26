bf922098f8f143602e1c70f5d5261457c43ade56
codypearce <codyapearce@gmail.com>
2017-05-25 23:44:18 -0600

    service in own file

--- js/services/contacts.js
+++ js/services/contacts.js
@@ -1,1 +1,24 @@
 
+app.service('contacts', function() {
+    this.contacts =  [
+      {
+        firstName: 'Luke',
+        lastName: 'Skywalker',
+        phone: '555-555-5555',
+        email: 'skywalkingoveru@rebels.io',
+        img: 'luke-skywalker.jpg',
+      },
+      {
+        firstName: 'Darth',
+        lastName: 'Vader',
+        phone: '555-555-5554',
+        email: 'idontlikesand@empire.io',
+        img: 'darth-vader.jpg',
+      },
+    ];
+
+    this.addContact = function(contact) {
+      this.contacts.push(contact);
+    }
+
+});
